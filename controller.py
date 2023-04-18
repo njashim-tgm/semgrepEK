ce503e776f381391ff546c0aa1b6b9b0cffffa13
Nahi Islam Jashim <njashim@student.tgm.ac.at>
2023-04-18 09:03:07 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -40,5 +40,4 @@ if __name__ == '__main__':
 exec("exec-test")
 eval("eval-test")
 
-txt = "For only {price:.2f} dollars!"
-print(txt.format(price = 49))
+print("Hello, I am {} years old !".format(18))
